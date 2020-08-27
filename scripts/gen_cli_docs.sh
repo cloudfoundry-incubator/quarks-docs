eab7ef9bcfd20ae1b1073be574c908cacb534139
Mario Manno <mario.manno@suse.com>
2020-08-27 14:06:45 +0200

    Remove slash from CI titles

--- scripts/gen_cli_docs.sh
+++ scripts/gen_cli_docs.sh
@@ -7,19 +7,19 @@ mkdir build/
 git clone https://github.com/cloudfoundry-incubator/quarks-operator build/operator
 pushd build/operator
     mkdir -p content/en/docs/quarks-operator/CLI
-    go run cmd/gen-command-docs.go content/en/docs/quarks-operator/CLI
+    go run cmd/gen-command-docs.go content/en/docs/quarks-operator/CLI/
 popd
 
 git clone https://github.com/cloudfoundry-incubator/quarks-job build/quarks-job
 pushd build/quarks-job
     mkdir -p content/en/docs/quarks-job/CLI
-    go run cmd/docs/gen-command-docs.go content/en/docs/quarks-job/CLI
+    go run cmd/docs/gen-command-docs.go content/en/docs/quarks-job/CLI/
 popd
 
 git clone https://github.com/cloudfoundry-incubator/quarks-secret build/quarks-secret
 pushd build/quarks-secret
     mkdir -p content/en/docs/quarks-secret/CLI
-    go run cmd/docs/gen-command-docs.go content/en/docs/quarks-secret/CLI
+    go run cmd/docs/gen-command-docs.go content/en/docs/quarks-secret/CLI/
 popd
 
 cp -rf build/operator/content/* content/
